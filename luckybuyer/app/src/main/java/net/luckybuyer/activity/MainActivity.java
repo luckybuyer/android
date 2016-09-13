d3885fbe53fa613897212302e088bf518753fd12
yangshuyu <810043299@qq.com>
2016-09-13 13:21:50 +0800

    新建工程

---
+++ luckybuyer/app/src/main/java/net/luckybuyer/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package net.luckybuyer.activity;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.luckybuyer.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
